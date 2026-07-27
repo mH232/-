e2085f2d5b8a67123c72bac8d799bd4a09adc7f8
Mohkadem <s202245820@kfupm.edu.sa>
2026-07-27 12:20:47 +0300

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">عَدْل</div>
+      <div className="main-sections">
+        <ul>
+          <li>من نحن</li>
+          <li>الخدمات</li>
+          <li>تواصل معنا</li>
+          <div className="login-button">تسجيل الدخول</div>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
