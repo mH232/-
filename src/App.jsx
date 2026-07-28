1355d288398aa2c3f36e140b9f21ae60fe312637
Mohkadem <s202245820@kfupm.edu.sa>
2026-07-28 23:19:57 +0300

    Header is completed & Paths for login page and Home page are prepared for latter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Header from "./Components/Header";
+
 function App() {
   return (
-    <div>
+    <div className="h-[5000px]">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,29 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <div className="header">
-      <div className="logo">عَدْل</div>
-      <div className="main-sections">
-        <ul>
-          <li>من نحن</li>
-          <li>الخدمات</li>
-          <li>تواصل معنا</li>
-          <div className="login-button">تسجيل الدخول</div>
-        </ul>
+    <header className="sticky top-4 z-50">
+      <div className="mx-auto w-[90%] max-w-7xl rounded-2xl border border-white/20 bg-white/20 backdrop-blur-md shadow-lg">
+        <div className="flex items-center justify-between px-6 py-4 ">
+          {/* <div className="login-button cursor-pointer bg-secondary text-white w-35 h-13.75 rounded-[10px] flex justify-center items-center text-xl"></div> */}
+          <Link
+            to={"/login"}
+            className="login-button cursor-pointer bg-secondary text-white w-35 h-13.75 rounded-[10px] flex justify-center items-center text-xl"
+          >
+            تسجيل الدخول
+          </Link>
+          <nav>
+            <ul className="flex gap-8 text-secondary text-xl">
+              <li className="cursor-pointer">تواصل معنا</li>
+              <li className="cursor-pointer">الخدمات</li>
+              <li className="cursor-pointer">من نحن</li>
+            </ul>
+          </nav>
+
+          <div className="logo text-4xl font-bold text-main">عَدْل</div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
