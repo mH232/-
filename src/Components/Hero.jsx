de51b80ce3dde18c7f4e41a4f6283c6de1a36144
Mohkadem <s202245820@kfupm.edu.sa>
2026-07-29 11:54:59 +0300

    Hero section is done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-import Header from "./Header";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+gsap.registerPlugin(SplitText);
 function Hero() {
-  return <div className=" h-screen"></div>;
+  useGSAP(() => {
+    const split = SplitText.create(".txt", {
+      type: "words",
+    });
+    gsap.from(split.words, {
+      y: 100,
+      autoAlpha: 0,
+      stagger: 0.05,
+    });
+  });
+
+  return (
+    <div className=" h-screen flex-center">
+      <div className="mx-auto w-[90%] max-w-7xl flex justify-between items-center">
+        <img src="../../src/assets/LandingPage.png" alt="Landing Page Image" />
+        <p dir="rtl" className="txt text-5xl leading-16 text-main">
+          رؤية شاملة لمؤشرات الأداء
+          <br />
+          لدعم قرارات مالية أكثر كفاءة
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
